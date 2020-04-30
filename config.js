fd5bcfa7d6fbd559553b3d52cd86a7b8208705a5
youngbinha <guseowhtjs@gmail.com>
2020-04-30 13:46:46 +0900

    "basic settings"

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://guseowhtjs.github.io/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Welcome to Ha's blog',
+  subtitle: '안녕하세요 하영빈의 블로그에 오신 것을 환영합니다!!행복한 하루 되세요!',
   copyright: '© All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
+  postsPerPage: 10,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
@@ -25,15 +25,15 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'John ha',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: '',
+      email: 'guseowhtjs@gmail.com',
       facebook: '#',
       telegram: '#',
       twitter: '#',
-      github: '#',
+      github: 'github.com/guseowhtjs1231#',
       rss: '',
       vkontakte: '',
       linkedin: '#',
